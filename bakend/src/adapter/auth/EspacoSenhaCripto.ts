9df5956b82a536105de1159f9250bb552d39c610
Ruan Tarcisio <ruantarciisio@gmail.com>
2024-12-11 13:27:19 -0300

    finalizado, feitas alteracoes.

--- bakend/src/adapter/auth/EspacoSenhaCripto.ts
+++
@@ -1,8 +0,0 @@
-import ProvedorCriptografia from "@/core/usuario/service/ProvedorCriptografia";
-
-export default class EspacoSenhaCripto implements ProvedorCriptografia {
-
-    criptografar(texto: string): string {
-        return texto.split('').join(' ')
-    }
-}
